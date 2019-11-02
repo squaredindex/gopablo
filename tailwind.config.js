e975650c5ea7ad59a8099f7c439dfe8d260d05ed
Lev <levdevuk@gmail.com>
2019-11-02 14:44:03 +0000

    Added Tailwind Config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  prefix: '',
+  important: false,
+  separator: ':',
+  theme: {
+    fontFamily: {
+        display: ['Poppins', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+      },
+    extend: {
+        colors: {
+            brand: '#DC5A2C',
+        }
+    }
+  },
+  variants: {},
+  plugins: []
+};
